0d1e499b7903adda486fb6058a5a21d24d393cae
ingofreak <33750816+ingofreak@users.noreply.github.com>
2018-03-23 14:18:43 +0000

    Add files via upload

---
+++ regextest.tpl
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="is">
+<head>
+	<title>Sign Up</title>
+
+</head>
+<body>
+	<form method='post' action='data' accept-charset="ISO-8859-1">
+	  E-mail:<br>
+      <input type="text" name='email' pattern="(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)" required><br>
+	  Username:<br>
+      <input type="text" name='user' pattern="(?=.*[a-z])[a-zA-Z]{4,}" required><br>
+      Password:<br>
+      <input type="text" name='pass' pattern= "(?=.*[a-z])[a-zA-Z]{4,}" required><br>
+      <input type='submit' value='Panta'>
+  	  <input type='reset' value='Hreinsa'>
+	</form>
+
+</body>
+</html>
---
+++ verkefni6b.py
@@ -0,0 +1,32 @@
+from bottle import *
+import json
+import os
+
+@route('/')
+def index():
+    return template('regextest')
+
+@post('/data')
+def data():
+    em = request.forms.get('email')
+    us = request.forms.get('user')
+    pw = request.forms.get('pass')
+    
+
+    skra = open('user.json','r')
+    users = json.load(skra)
+    
+    skra.close()
+    
+    #Bætum nýjum við...
+    """users["user"].append({"u":u,"p":p,"e":m})
+    print(users)
+    skra1 = open('user.json','w')
+    json.dump(users,skra1)
+    skra1.close()"""
+    return us , " hefur veirð bætt við..."
+
+
+
+
+run(host='localhost', port=8080)
